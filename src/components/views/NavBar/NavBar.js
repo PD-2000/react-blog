6467333aad97c5f96cc3f68ade74685f3f2f23be
Pawel Domagala <domagalapawel11@gmail.com>
2023-05-03 21:57:42 +0200

    Add dropdown menu for mobile

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
@@ -6,16 +6,17 @@ const NavBar = () => {
 		<Navbar
 			bg="primary"
 			variant="dark"
-			expand="lg"
-			className="mt-4 mb-4 rounded px-3"
+			expand="md"
+			className="mt-4 mb-4 rounded px-3 justify-content-between"
 		>
-			<Container>
-				<Navbar.Brand>Blog.app</Navbar.Brand>
-				<Nav>
-					<Nav.Link as={NavLink} to="/">Home</Nav.Link>
-					<Nav.Link as={NavLink} to="/about">About</Nav.Link>
-				</Nav>
-			</Container>
+			<Navbar.Brand>Blog.app</Navbar.Brand>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+			<Nav className="ms-auto flex-md-row">
+				<Nav.Link as={NavLink} to="/">Home</Nav.Link>
+				<Nav.Link as={NavLink} to="/about">About</Nav.Link>
+			</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	);
 };
