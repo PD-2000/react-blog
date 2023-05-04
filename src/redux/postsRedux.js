b3cce7086f119102228d696e7a922f53afa1fe97
Pawel Domagala <domagalapawel11@gmail.com>
2023-05-04 19:53:15 +0200

    Add Edit and Remove to SinglePost

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -4,10 +4,16 @@ export const getPostById = ({posts}, postId) => posts.find(post => post.id === p
 
 // Actions
 const createActionName = actionName => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 // Action creators
+export const removePost = payload => ({type: REMOVE_POST, payload});
+
+// Reducer
 const postsReducer = (statePart = [], action) => {
 	switch(action.type){
+		case REMOVE_POST:
+			return [...statePart.filter(post => post.id !== action.payload)];
 		default:
 			return statePart;
 	};
